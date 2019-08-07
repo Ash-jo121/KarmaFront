982b4beaa83f34838262661fa1bfbebedc1d0371
Jacob Abraham <jacob13400@gmail.com>
2019-08-07 08:20:31 +0530

    Tweaks

---
+++ my-app/src/Admission.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dropdown from './Dropdown3'
+import Navigator from './Navigator'
+import { BrowserRouter} from 'react-router-dom';
+
+class Admission extends React.Component {
+
+render(){
+    return(
+        <div>
+            <h2>Admission</h2>
+        </div>
+    );
+}
+}
+ export default Admission;
--- my-app/src/Navigator.js
+++ my-app/src/Navigator.js
@@ -3,6 +3,7 @@ import './Navigator.css';
 import './App.css'
 import Home from './Home'
 import Attendance from './Attendance'
+import Admission from './Admission'
 import { Redirect } from 'react-router'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -18,15 +19,16 @@ export default class Navigator extends React.Component {
         <div class="sidenav">
                 <nav>
                     <Link to="/">Dashboard</Link>
-                    <Link to="/Attendance">Attendance</Link>
-                    <a href="#clients">Admission</a>
+                    <Link to="/attendance">Attendance</Link>
+                    <Link to="/admission">Admission</Link>
                     <a href="#contact">Contact</a>
                 </nav>
         </div>
 
         <div class ="baseInNav">        
             <Switch>
-                <Route path='/Attendance' component={Attendance} />
+                <Route path='/attendance' component={Attendance} />                
+                <Route path='/admission' component={Admission} />
                 <Route path='/' component={Home} />
             </Switch>
         </div>
